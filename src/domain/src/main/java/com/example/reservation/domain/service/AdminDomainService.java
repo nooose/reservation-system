9462446af191946b4015f10247f04ef1fd13ed99
dltkqnr <dltkqnr@naver.com>
2021-12-08 19:18:40 +0900

    Admin 도메인

---
+++ src/domain/src/main/java/com/example/reservation/domain/service/AdminDomainService.java
@@ -0,0 +1,55 @@
+package com.example.reservation.domain.service;
+
+import com.example.reservation.domain.entity.Admin;
+import com.example.reservation.domain.entity.Member;
+import com.example.reservation.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AdminDomainService implements MemberDomainService{
+
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void saveMember(Member member) {
+        Admin admin = (Admin) member;
+        memberRepository.save(admin);
+        log.info("{} 어드민 저장", admin.getName());
+    }
+
+    @Override
+    public Member getMember(Long id) {
+        Optional<Member> byId = memberRepository.findById(id);
+        Member member = byId.orElseThrow(()-> new IllegalArgumentException("특정 회원이 없습니다."));
+        return member;
+    }
+
+    @Override
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Override
+    public void updateMember(Long id, Member member) {
+
+        Admin findAdmin = (Admin) getMember(id);
+
+        findAdmin.changeNickName(member.getNickName());
+        findAdmin.changeAddress(member.getAddress());
+        findAdmin.changePassword(member.getPassword());
+        findAdmin.changePhoneNumber(member.getPhoneNumber());
+    }
+
+    @Override
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
+}
