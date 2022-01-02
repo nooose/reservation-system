a6a64b8cfafe9eda1afac9ae47eb28be6e22d63e
junhyuksung <test@test.com>
2022-01-02 15:03:31 +0900

    LogInterceptor 추가

---
+++ src/presentation/src/main/java/com/example/reservation/controller/web/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.reservation.controller.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+}
